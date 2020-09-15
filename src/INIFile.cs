fae42277a888467b8db40feedc12714e7394ed1d
AgarCdF <ambroise.garel@gmail.com>
2020-09-15 17:21:14 +0200

    Added comments and overrideable protected conversion methods

--- src/INIFile.cs
+++ src/INIFile.cs
@@ -19,36 +19,53 @@ along with INIPlusPlus. If not, see https://www.gnu.org/licenses/
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
 namespace INIPlusPlus
 {
-    public sealed class INIFile : IDisposable
+    /// <summary>
+    /// An .INI file
+    /// </summary>
+    public class INIFile : IDisposable
     {
+        /// <summary>
+        /// Dictionary of all sections in this INI file.
+        /// </summary>
         private readonly Dictionary<string, INIFileSection> Sections = new Dictionary<string, INIFileSection>(StringComparer.InvariantCultureIgnoreCase);
 
-        public INIFile()
-        {
-            Clear();
-        }
+        /// <summary>
+        /// Constructor. Creates an empty file with no sections or values.
+        /// </summary>
+        public INIFile() { Clear(); }
 
-        public INIFile(string filePath)
+        /// <summary>
+        /// Constructor. Load data from an INI file.
+        /// </summary>
+        /// <param name="filePath">Path to the INI file</param>
+        /// <param name="encoding">Text encoding to use. Default is UTF-8</param>
+        public INIFile(string filePath, Encoding encoding = null)
         {
             Clear();
 
             if (!File.Exists(filePath)) return;
-            string iniString = File.ReadAllText(filePath, Encoding.UTF8);
+            string iniString = File.ReadAllText(filePath, encoding ?? Encoding.UTF8);
             ParseINIString(iniString);
         }
 
-        public string GetSectionParentSection(string section)
-        {
-            if (!Sections.ContainsKey(section)) return "";
+        //public string GetSectionParentSection(string section)
+        //{
+        //    if (!Sections.ContainsKey(section)) return "";
 
-            return Sections[section].ParentSection ?? "";
-        }
+        //    return Sections[section].ParentSection ?? "";
+        //}
 
+        /// <summary>
+        /// Creates an instance of INIFile from a raw INI string
+        /// </summary>
+        /// <param name="iniString">String containing INI data</param>
+        /// <returns>An INIFile</returns>
         public static INIFile CreateFromRawINIString(string iniString)
         {
             INIFile ini = new INIFile();
@@ -57,11 +74,17 @@ namespace INIPlusPlus
             return ini;
         }
 
+        /// <summary>
+        /// IDisposable implementation.
+        /// </summary>
         public void Dispose()
         {
             Clear();
         }
 
+        /// <summary>
+        /// Clears all sections and values.
+        /// </summary>
         public void Clear()
         {
             foreach (string s in Sections.Keys)
@@ -70,7 +93,13 @@ namespace INIPlusPlus
             Sections.Clear();
         }
 
-        public bool SaveToFile(string filePath)
+        /// <summary>
+        /// Saves the current state of the INIFile to a file.
+        /// </summary>
+        /// <param name="filePath">Path to the file to write</param>
+        /// <param name="encoding">Text encoding to use. Default is UTF-8</param>
+        /// <returns>True if everything went right, false if an error happened</returns>
+        public bool SaveToFile(string filePath, Encoding encoding = null)
         {
             try
             {
@@ -92,7 +121,7 @@ namespace INIPlusPlus
                     fileContent += "\r\n";
                 }
 
-                File.WriteAllText(filePath, fileContent, Encoding.UTF8);
+                File.WriteAllText(filePath, fileContent, encoding ?? Encoding.UTF8);
 
                 return true;
             }
@@ -102,16 +131,24 @@ namespace INIPlusPlus
             }
         }
 
-        public bool IsSectionAbstract(string section)
-        {
-            return Sections.ContainsKey(section) && Sections[section].Abstract;
-        }
 
+        /// <summary>
+        /// Returns the names of all non-abstract sections.
+        /// </summary>
+        /// <returns>A string array</returns>
         public string[] GetSections()
         {
-            return Sections.Keys.OrderBy(x => x).ToArray();
+            return (from string s in Sections.Keys where !Sections[s].Abstract select s).OrderBy(x => x).ToArray();
         }
 
+        /// <summary>
+        /// Reads and returns a value from the ini file.
+        /// </summary>
+        /// <typeparam name="T">Type of the value to read</typeparam>
+        /// <param name="section">Section in which to read the value</param>
+        /// <param name="key">Key of the value</param>
+        /// <param name="defaultValue">Default value to return if the values doesn't exist or is invalid</param>
+        /// <returns>A value</returns>
         public T GetValue<T>(string section, string key, T defaultValue = default)
         {
             if (!ValueExists(section, key))
@@ -120,18 +157,14 @@ namespace INIPlusPlus
                 return defaultValue;
             }
 
-            object val = ReadValue(section, key) ?? "";
+            string val = ReadValue(section, key) ?? "";
 
             try
             {
-                if (typeof(T) == typeof(string)) val = (string)val;
-                else if (typeof(T) == typeof(bool)) val = ConversionTools.StringToBool((string)val);
-                else if (typeof(T) == typeof(int)) val = ConversionTools.StringToInt((string)val);
-                else if (typeof(T) == typeof(float)) val = ConversionTools.StringToFloat((string)val);
-                else if (typeof(T) == typeof(double)) val = ConversionTools.StringToDouble((string)val);
-                else if (typeof(T).IsEnum) val = Enum.Parse(typeof(T), (string)val, true);
-
-                return (T)Convert.ChangeType(val, typeof(T));
+                if (CanConvertStringTo<T>())
+                    return ConvertStringTo(val, defaultValue);
+                else
+                    return default;
             }
             catch (Exception)
             {
@@ -139,6 +172,14 @@ namespace INIPlusPlus
             }
         }
 
+        /// <summary>
+        /// Reads and returns an array of value from the ini file.
+        /// </summary>
+        /// <typeparam name="T">Type of the value to read</typeparam>
+        /// <param name="section">Section in which to read the value</param>
+        /// <param name="key">Key of the value</param>
+        /// <param name="separator">Separator character between values in the array</param>
+        /// <returns>An array of values</returns>
         public T[] GetValueArray<T>(string section, string key, char separator = ',')
         {
             if (string.IsNullOrEmpty(GetValue<string>(section, key))) return new T[0];
@@ -162,16 +203,19 @@ namespace INIPlusPlus
             }
         }
 
-        public void SetValue<T>(string section, string key, T val)
+        /// <summary>
+        /// Sets a value in the INI file
+        /// </summary>
+        /// <typeparam name="T">Type of the value to set</typeparam>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void SetValue<T>(string section, string key, T value)
         {
-            object oVal = val;
-
-            if (typeof(T) == typeof(string)) WriteValue(section, key, (string)oVal);
-            else if (typeof(T) == typeof(bool)) WriteValue(section, key, ConversionTools.ValToString((bool)oVal));
-            else if (typeof(T) == typeof(int)) WriteValue(section, key, ConversionTools.ValToString((int)oVal));
-            else if (typeof(T) == typeof(float)) WriteValue(section, key, ConversionTools.ValToString((float)oVal));
-            else if (typeof(T) == typeof(double)) WriteValue(section, key, ConversionTools.ValToString((double)oVal));
-            else WriteValue(section, key, val.ToString());
+            if (CanConvertStringFrom<T>())
+                WriteValue(section, key, ConvertStringFrom(value));
+            else
+                WriteValue(section, key, "");
         }
 
         public void SetValueArray<T>(string section, string key, T[] val, char separator = ',')
@@ -218,6 +262,12 @@ namespace INIPlusPlus
         //    return keys.Distinct().ToArray();
         //}
 
+        /// <summary>
+        /// Does the value exist?
+        /// </summary>
+        /// <param name="section">Section in which the value is stored</param>
+        /// <param name="key">Value key</param>
+        /// <returns>True if the value exists, false if it doesn't</returns>
         public bool ValueExists(string section, string key)
         {
             return ReadValue(section, key) != null;
@@ -247,6 +297,13 @@ namespace INIPlusPlus
             return null;
         }
 
+        /// <summary>
+        /// Write a value to the INI file
+        /// </summary>
+        /// <param name="section">Section to write to</param>
+        /// <param name="key">Key to write</param>
+        /// <param name="value">Value</param>
+        /// <returns>True if written successfully, false otherwise</returns>
         private bool WriteValue(string section, string key, string value)
         {
             section = (section ?? "").ToLowerInvariant().Trim();
@@ -262,6 +319,10 @@ namespace INIPlusPlus
             return true;
         }
 
+        /// <summary>
+        /// Parses a INI string to populate sections, keys and values.
+        /// </summary>
+        /// <param name="iniString">A string containing INI data</param>
         private void ParseINIString(string iniString)
         {
             string[] lines = (iniString + "\n").Replace("\r\n", "\n").Split('\n');
@@ -320,6 +381,79 @@ namespace INIPlusPlus
             }
         }
 
+        protected virtual bool CanConvertStringTo<T>()
+        {
+            Type type = typeof(T);
+
+            if (
+                (type == typeof(bool)) ||
+                (type == typeof(double)) ||
+                (type == typeof(float)) ||
+                (type == typeof(int)) ||
+                (type == typeof(string)) ||
+                type.IsEnum)
+                return true;
+
+            return false;
+        }
+
+        protected virtual bool CanConvertStringFrom<T>()
+        {
+            Type type = typeof(T);
+
+            if (
+                (type == typeof(bool)) ||
+                (type == typeof(double)) ||
+                (type == typeof(float)) ||
+                (type == typeof(int)) ||
+                (type == typeof(string)) ||
+                type.IsEnum)
+                return true;
+
+            return false;
+        }
+
+        protected virtual T ConvertStringTo<T>(string value, T defaultValue = default)
+        {
+            Type type = typeof(T);
+
+            object outObject = defaultValue;
+
+            if (type == typeof(bool))
+                try { outObject = Convert.ToBoolean(value, NumberFormatInfo.InvariantInfo); } catch (Exception) { }
+            else if (type == typeof(double))
+                try { outObject = Convert.ToDouble(value, NumberFormatInfo.InvariantInfo); } catch (Exception) { }
+            else if (type == typeof(float))
+                try { outObject = Convert.ToSingle(value, NumberFormatInfo.InvariantInfo); } catch (Exception) { }
+            else if (type == typeof(int))
+                try { outObject = Convert.ToInt32(value, NumberFormatInfo.InvariantInfo); } catch (Exception) { }
+            else if (type == typeof(string))
+                outObject = value;
+            else if (type.IsEnum)
+                try { outObject = Enum.Parse(typeof(T), value, true); } catch (Exception) { }
+
+            return (T)outObject;
+        }
+
+        protected virtual string ConvertStringFrom<T>(T value)
+        {
+            Type type = typeof(T);
+
+            object inObject = value;
+            string outString = value.ToString();
+
+            if (type == typeof(bool))
+                outString = ((bool)inObject).ToString(NumberFormatInfo.InvariantInfo);
+            else if (type == typeof(double))
+                outString = ((double)inObject).ToString(NumberFormatInfo.InvariantInfo);
+            else if (type == typeof(float))
+                outString = ((float)inObject).ToString(NumberFormatInfo.InvariantInfo);
+            else if (type == typeof(int))
+                outString = ((int)inObject).ToString(NumberFormatInfo.InvariantInfo);
+
+            return outString;
+        }
+
         private T[] ConvertArray<T>(string[] sourceArray)
         {
             try
@@ -328,15 +462,10 @@ namespace INIPlusPlus
 
                 for (int i = 0; i < sourceArray.Length; i++)
                 {
-                    object o = default(T);
-
-                    if (typeof(T) == typeof(bool)) o = ConversionTools.StringToBool(sourceArray[i]);
-                    else if (typeof(T) == typeof(int)) o = ConversionTools.StringToInt(sourceArray[i]);
-                    else if (typeof(T) == typeof(double)) o = ConversionTools.StringToDouble(sourceArray[i]);
-                    else if (typeof(T) == typeof(float)) o = ConversionTools.StringToFloat(sourceArray[i]);
-                    else if (typeof(T).IsEnum) o = Enum.Parse(typeof(T), sourceArray[i].ToString(), true);
-
-                    arr[i] = (T)Convert.ChangeType(o, typeof(T));
+                    if (CanConvertStringTo<T>())
+                        arr[i] = ConvertStringTo<T>(sourceArray[i]);
+                    else
+                        arr[i] = default;
                 }
 
                 return arr;
