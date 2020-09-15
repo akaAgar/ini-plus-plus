0e5504333e3a179e88b9cf35c99bbca16202d914
AgarCdF <ambroise.garel@gmail.com>
2020-09-15 20:08:43 +0200

    Renamed Program.cs to DemoProgram.cs

--- src/INIPlusPlusDemo/DemoProgram.cs
+++ src/INIPlusPlusDemo/DemoProgram.cs
@@ -1,9 +1,33 @@
-﻿using System;
+﻿/*
+==========================================================================
+This file is part of INIPlusPlus, an advanced cross-platform INI parser
+handling abstract sections and inheritance, by @akaAgar
+(https://github.com/akaAgar/ini-plus-plus)
+INIPlusPlus is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+INIPlusPlus is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with INIPlusPlus. If not, see https://www.gnu.org/licenses/
+==========================================================================
+*/
+
+using System;
 
 namespace INIPlusPlus.Demo
 {
+    /// <summary>
+    /// Demo program. Loads data from a demo INI file and displays them in the console.
+    /// </summary>
     public sealed class DemoProgram
     {
+        /// <summary>
+        /// Static Main() method.
+        /// </summary>
         private static void Main()
         {
 #if DEBUG
